b8d98d15751753ee6a178b8feb0efc51622bec88
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2023-10-25 16:31:15 +0300

    move OrderInfoItem class to new file

--- lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:checkout_payment/core/utils/styles.dart';
+import 'package:checkout_payment/features/checkout/presentation/views/widgets/cart_info_item.dart';
 import 'package:checkout_payment/images/app_images.dart';
 import 'package:flutter/material.dart';
 
@@ -38,30 +38,6 @@ class MyCartViewBody extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class OrderInfoItem extends StatelessWidget {
-  const OrderInfoItem({super.key, required this.title, required this.value});
-  final String title, value;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          title,
-          textAlign: TextAlign.center,
-          style: Styles.style18,
-        ),
-        const Spacer(),
-        Text(
-          value,
-          textAlign: TextAlign.center,
-          style: Styles.style18,
-        ),
-      ],
     );
   }
 }
