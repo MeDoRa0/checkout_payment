901ba76f73a0fbd250e40dd66f79b82b65db83a9
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2023-10-25 16:28:46 +0300

    add order subtototal item

--- lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:checkout_payment/core/utils/styles.dart';
 import 'package:checkout_payment/images/app_images.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +7,61 @@ class MyCartViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [Image.asset(Assets.imagesShoppingCart)],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 15,
+          ),
+          Image.asset(Assets.imagesShoppingCart),
+          const SizedBox(
+            height: 25,
+          ),
+          const OrderInfoItem(
+            title: 'Order Subtotal',
+            value: '\$42.97',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Discount',
+            value: '\$0',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Shipping',
+            value: '\$8',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OrderInfoItem extends StatelessWidget {
+  const OrderInfoItem({super.key, required this.title, required this.value});
+  final String title, value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: Styles.style18,
+        ),
+        const Spacer(),
+        Text(
+          value,
+          textAlign: TextAlign.center,
+          style: Styles.style18,
+        ),
+      ],
     );
   }
 }
