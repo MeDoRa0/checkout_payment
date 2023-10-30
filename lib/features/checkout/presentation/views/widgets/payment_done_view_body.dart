bfac9899769031f9046c955746480c0fbd8e053b
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2023-10-31 00:00:38 +0200

    custom shaped container

--- lib/features/checkout/presentation/views/widgets/payment_done_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_done_view_body.dart
@@ -7,8 +7,10 @@ class PaymentDoneViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     // Stack widget enable us to put widget on widget
     return Padding(
-      padding: const EdgeInsets.all(32),
+      padding: const EdgeInsets.all(20),
       child: Stack(
+        //this code prevent clip
+        clipBehavior: Clip.none,
         children: [
           Container(
             decoration: ShapeDecoration(
@@ -33,6 +35,23 @@ class PaymentDoneViewBody extends StatelessWidget {
             bottom: MediaQuery.sizeOf(context).height * .2,
             child: const CircleAvatar(
               backgroundColor: Colors.white,
+            ),
+          ),
+          const Positioned(
+            left: 0,
+            right: 0,
+            top: -40,
+            child: CircleAvatar(
+              radius: 40,
+              backgroundColor: Color(0xFFD9D9D9),
+              child: CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.green,
+                child: Icon(
+                  Icons.check,
+                  size: 45,
+                ),
+              ),
             ),
           ),
         ],
