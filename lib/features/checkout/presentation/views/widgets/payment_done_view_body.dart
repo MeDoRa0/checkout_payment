33db94ced905a829bb44f72fa100bbcbe284b40b
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2023-11-01 16:26:42 +0200

    creat dashed line

--- lib/features/checkout/presentation/views/widgets/payment_done_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_done_view_body.dart
@@ -20,6 +20,29 @@ class PaymentDoneViewBody extends StatelessWidget {
               ),
             ),
           ),
+          //this code for dash line
+          Positioned(
+            bottom: MediaQuery.sizeOf(context).height * .2 + 20,
+            left: 20 + 6,
+            right: 20 + 6,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 7),
+              child: Row(
+                children: List.generate(
+                  20,
+                  (index) => Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 2),
+                      child: Container(
+                        height: 3,
+                        color: const Color(0xffB8B8B8),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
           //this code will put the circle in postion we want
           Positioned(
             left: -20,
