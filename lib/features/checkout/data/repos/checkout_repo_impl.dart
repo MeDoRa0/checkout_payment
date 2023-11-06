29a1fea2be5b0ab4b164bb246ed535ec961fb0b1
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2023-11-06 16:05:22 +0200

    creat check out implementaion class

---
+++ lib/features/checkout/data/repos/checkout_repo_impl.dart
@@ -0,0 +1,20 @@
+import 'package:checkout_payment/core/errors/failures.dart';
+import 'package:checkout_payment/core/utils/stripe_service.dart';
+import 'package:checkout_payment/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_payment/features/checkout/data/repos/check_out_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class CheckOutRepoImpl extends CheckOutRepo {
+  final StripeService stripeService = StripeService();
+  @override
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    try {
+      await stripeService.makePayment(
+          paymentIntentInputModel: paymentIntentInputModel);
+      return right(null);
+    } catch (e) {
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+}
